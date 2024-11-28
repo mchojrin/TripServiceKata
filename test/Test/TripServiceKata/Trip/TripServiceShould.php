2fac64665dfb44388356297b1ebd5b1e3cbb401c
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-11-28 17:19:54 +0100

    First test, failing for wrong exception class

--- test/Test/TripServiceKata/Trip/TripServiceShould.php
+++ test/Test/TripServiceKata/Trip/TripServiceShould.php
@@ -3,7 +3,21 @@
 namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
+use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\Trip\TripService;
+use TripServiceKata\User\User;
 
 class TripServiceShould extends TestCase
 {
+    const FRIEND_NAME = "A friend";
+
+    /**
+     * @test
+     */
+    public function throw_exception_if_user_is_not_logged_in(): void
+    {
+        $this->expectException(UserNotLoggedInException::class);
+        $tripService = new TripService();
+        $tripService->getTripsByUser(new User(self::FRIEND_NAME));
+    }
 }
