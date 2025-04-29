91a2b2394af2b3c2e01c0790815b0a740e7a5a0a
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-04-29 15:04:51 +0200

    First running test

--- test/TripServiceKata/Trip/TripServiceShould.php
+++ test/TripServiceKata/Trip/TripServiceShould.php
@@ -1,9 +1,18 @@
 <?php
 
-namespace test\TripServiceKata\Trip;
+namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
+use TripServiceKata\Trip\TripService;
 
 class TripServiceShould extends TestCase
 {
+    /**
+     * @test
+     */
+    public function do_something(): void
+    {
+        $tripService = new TripService();
+        $this->assertEquals([], $tripService->getTripsByUser(null));
+    }
 }
